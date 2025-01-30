4ddeb0681b076abc40054ca01f2855ed10154717
Benjamín R. Moreno Ortiz <benjamin_moreno_ortiz@alumni.brown.edu>
2025-01-30 13:43:05 -0600

    Create Create RDC-Enero30-2.glsl

---
+++ Create RDC-Enero30-2.glsl
@@ -0,0 +1,21 @@
+#ifdef GL_ES
+precision medium float ;
+#endif
+
+#define PROCESSING_COLOR_SHADER
+
+uniform float t;
+uniform vec2 res;
+uniform vec2 mouse;
+uniform vec3 color1;
+uniform vec3 color2;
+
+
+
+void main(){
+    vec2 pos = gl_FragCoord.xy/res;
+    vec2 centro = vec2(0.5);
+    float distancia = sin(-t*0.01+distance(centro,pos)*20.0)*0.5+0.5;
+    vec3 rgb = mix(color1,color2,distancia);
+    gl_FragColor = vec4(rgb,1.0);
+}
